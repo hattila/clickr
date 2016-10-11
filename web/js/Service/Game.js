34eb13e7b2287393728d925ff09b5b4d8ef98e10
Attila Hejjas <attila.h9@gmail.com>
2016-10-11 22:52:19 +0200

    timed monster spawning.

--- web/js/Service/Game.js
+++ web/js/Service/Game.js
@@ -8,20 +8,62 @@ Hw.Srvc.Game = Hw.Srvc.Game || (function(){
      * Properties
      */
     var _monsters = 10;
+    var _monsterSpawningActive = false;
+    var _monsterTmp = $('#monster-template').html();
 
     var init = function ()
     {
-        
-        var tmp = $('#monster-template').html();
-        
+
+        // var mob = Hw.Srvc.Spawner.spawnRandom();
+        // $('#game-field').append(_getMonsterTmp(mob));
+
+        _spawnMonstersToTheField(3, 1000);
+
         /**
          * Subscribe demo
          */
-        $.subscribe('/something/happens', function(e, ele){
+        $.subscribe('/monster/dies', function(e, ele){
             
         });
     };
 
+    /**
+     *
+     * @param numberOfMonsters
+     * @param interval [5000]
+     */
+    var _spawnMonstersToTheField = function (numberOfMonsters, interval) {
+        interval = interval !== undefined ? interval : 5000;
+
+        if (!_monsterSpawningActive) {
+            _monsterSpawningActive = true;
+            var idx = 0;
+
+            var intervalVar = setInterval(function(){
+                if (idx == numberOfMonsters) {
+                    clearTimeout(intervalVar);
+                }
+                var mob = Hw.Srvc.Spawner.spawnRandom();
+
+                $('#game-field').append(_getMonsterTmp(mob));
+
+                idx++;
+            }, interval);
+        }
+
+    };
+
+    var _getMonsterTmp = function (monster) {
+        var newTmp = _monsterTmp;
+
+        console.log(newTmp);
+        console.log(monster.getName());
+
+        newTmp = newTmp.replace('{maxHp}', monster.getMaxHp());
+        newTmp = newTmp.replace('{hp}', monster.getHp());
+        newTmp = newTmp.replace('{name}', monster.getName());
+        return newTmp;
+    };
 
     return {
         init: init
