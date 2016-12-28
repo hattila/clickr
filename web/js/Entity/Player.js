6d138d9cf5bf21ce260385f98b41439a37cc2021
Héjjas Attila <attila.h9@gmail.com>
2016-12-28 18:43:34 +0100

    add dom update functions for Player.

--- web/js/Entity/Player.js
+++ web/js/Entity/Player.js
@@ -8,29 +8,37 @@ Hw.Enty.Player = Hw.Enty.Player || (function(){
      * @private
      */
     var _basedamage = 1;
+    var _damage = _basedamage;
     var _stamina = 100;
     var _sanity = 100;
     var _xp = 0;
     var _level = 1;
 
-    var _statsContainerId = 'stats';
+    var _statsContainerId = '#stats';
     var _$stats = {
-        damage: $('#' + _statsContainerId + ' .damage'),
-        stamina: $('#' + _statsContainerId + ' .stamina'),
-        sanity: $('#' + _statsContainerId + ' .sanity'),
-        xp: $('#' + _statsContainerId + ' .xp'),
+        damage: $(_statsContainerId + ' .damage'),
+        stamina: $(_statsContainerId + ' .stamina'),
+        sanity: $(_statsContainerId + ' .sanity'),
+        xp: $(_statsContainerId + ' .xp')
+    };
+
+    var getBaseDamage = function () {
+        return _basedamage;
     };
 
     var getDamage = function () {
-        return _basedamage;
+        return _damage;
     };
 
     /**
      * Player receives damage
+     *
+     * @param damage
+     * @returns {number}
      */
     var recDamage = function (damage) {
         _stamina = damage < _stamina ? _stamina - damage : 0;
-        _updateStat('stamina', _stamina);
+        _updateStamina();
 
         if (0 === _stamina) {
             $.publish('/player/dies');
@@ -39,16 +47,63 @@ Hw.Enty.Player = Hw.Enty.Player || (function(){
         return _stamina;
     };
 
-    var _updateStat = function (stat, val) {
+    /**
+     * Player receives Sanity damage
+     *
+     * @param damage
+     * @returns {number}
+     */
+    var recSanityDamage = function (damage) {
+        _sanity = damage < _sanity ? _sanity - damage : 0;
+        _updateSanity();
+
+        if (0 === _sanity) {
+            $.publish('/player/goesInsane');
+        }
+
+        return _sanity;
+    };
+
+    /**
+     * One function to handle all the property DOM updates
+     *
+     * @param stat
+     * @returns {boolean}
+     * @private
+     */
+    var _updateStat = function (stat) {
         if ('undefined' === typeof _$stats[stat]) {
             console.warn('Stat does not exist on Player: ' + stat);
             return false;
         }
-        _$stats[stat].text(val);
-        return val;
+        // Cannot access the private property
+        // dynamic variable?
+        _$stats[stat].text(Player['_' + stat]);
+        return true;
     };
 
+    var _updateDamage = function () {
+        _$stats.damage.text(_damage);
+    };
+
+    var _updateStamina = function () {
+        _$stats.stamina.text(_stamina);
+    };
+
+    var _updateSanity = function () {
+        _$stats.sanity.text(_sanity);
+    };
+
+    var _updateXp = function () {
+        _$stats.xp.text(_xp);
+    };
+
+
     return {
-        getDamage: getDamage
+        getBaseDamage: getBaseDamage,
+        getDamage: getDamage,
+
+        recDamage: recDamage,
+        recSanityDamage: recSanityDamage
     }
 });
